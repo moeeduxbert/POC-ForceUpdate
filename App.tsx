f4cbd15fda8fbb80f097d2b1b9c72b97cefdc8af
Moeed Sarwar <moeed.c@uxbert.com>
2024-04-24 19:28:40 +0500

    Root Devices Detected

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import CodePush from 'react-native-code-push';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+      <Text>0.1.2</Text>
+    </View>
+  );
+};
+
+export default CodePush(App);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
